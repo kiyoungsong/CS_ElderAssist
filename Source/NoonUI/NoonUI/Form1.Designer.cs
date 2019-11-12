3fc975976753415817a0735efd7916f38953f006
kiyoungsong <thdrldud369@gmail.com>
2019-11-13 03:08:26 +0900

    modified sources

---
+++ Source/NoonUI/NoonUI/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NoonUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Closebt = new System.Windows.Forms.Button();
+            this.AlwaysTop = new System.Windows.Forms.Button();
+            this.Keyboard = new System.Windows.Forms.Button();
+            this.Narrator = new System.Windows.Forms.Button();
+            this.Magnify = new System.Windows.Forms.Button();
+            this.Eyetracker = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Closebt
+            // 
+            this.Closebt.BackColor = System.Drawing.Color.Transparent;
+            this.Closebt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Closebt.ForeColor = System.Drawing.Color.Transparent;
+            this.Closebt.Image = global::NoonUI.Properties.Resources._6f;
+            this.Closebt.Location = new System.Drawing.Point(511, 22);
+            this.Closebt.Name = "Closebt";
+            this.Closebt.Size = new System.Drawing.Size(94, 84);
+            this.Closebt.TabIndex = 5;
+            this.Closebt.UseVisualStyleBackColor = false;
+            this.Closebt.Click += new System.EventHandler(this.Closebt_Click);
+            // 
+            // AlwaysTop
+            // 
+            this.AlwaysTop.BackColor = System.Drawing.Color.Transparent;
+            this.AlwaysTop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AlwaysTop.ForeColor = System.Drawing.Color.Transparent;
+            this.AlwaysTop.Image = global::NoonUI.Properties.Resources._5f;
+            this.AlwaysTop.Location = new System.Drawing.Point(411, 22);
+            this.AlwaysTop.Name = "AlwaysTop";
+            this.AlwaysTop.Size = new System.Drawing.Size(94, 84);
+            this.AlwaysTop.TabIndex = 4;
+            this.AlwaysTop.UseVisualStyleBackColor = false;
+            this.AlwaysTop.Click += new System.EventHandler(this.AlwaysTop_Click);
+            // 
+            // Keyboard
+            // 
+            this.Keyboard.BackColor = System.Drawing.Color.Transparent;
+            this.Keyboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Keyboard.ForeColor = System.Drawing.Color.Transparent;
+            this.Keyboard.Image = global::NoonUI.Properties.Resources._4f;
+            this.Keyboard.Location = new System.Drawing.Point(311, 22);
+            this.Keyboard.Name = "Keyboard";
+            this.Keyboard.Size = new System.Drawing.Size(94, 84);
+            this.Keyboard.TabIndex = 3;
+            this.Keyboard.UseVisualStyleBackColor = false;
+            this.Keyboard.Click += new System.EventHandler(this.Keyboard_Click);
+            // 
+            // Narrator
+            // 
+            this.Narrator.BackColor = System.Drawing.Color.Transparent;
+            this.Narrator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Narrator.ForeColor = System.Drawing.Color.Transparent;
+            this.Narrator.Image = global::NoonUI.Properties.Resources._3f;
+            this.Narrator.Location = new System.Drawing.Point(211, 22);
+            this.Narrator.Name = "Narrator";
+            this.Narrator.Size = new System.Drawing.Size(94, 84);
+            this.Narrator.TabIndex = 2;
+            this.Narrator.UseVisualStyleBackColor = false;
+            this.Narrator.Click += new System.EventHandler(this.Narrator_Click);
+            // 
+            // Magnify
+            // 
+            this.Magnify.BackColor = System.Drawing.Color.Transparent;
+            this.Magnify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Magnify.ForeColor = System.Drawing.Color.Transparent;
+            this.Magnify.Image = global::NoonUI.Properties.Resources._2f;
+            this.Magnify.Location = new System.Drawing.Point(111, 22);
+            this.Magnify.Name = "Magnify";
+            this.Magnify.Size = new System.Drawing.Size(94, 84);
+            this.Magnify.TabIndex = 1;
+            this.Magnify.UseVisualStyleBackColor = false;
+            this.Magnify.Click += new System.EventHandler(this.Magnify_Click);
+            // 
+            // Eyetracker
+            // 
+            this.Eyetracker.BackColor = System.Drawing.Color.Transparent;
+            this.Eyetracker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Eyetracker.ForeColor = System.Drawing.Color.Transparent;
+            this.Eyetracker.Image = global::NoonUI.Properties.Resources._1f;
+            this.Eyetracker.Location = new System.Drawing.Point(11, 22);
+            this.Eyetracker.Name = "Eyetracker";
+            this.Eyetracker.Size = new System.Drawing.Size(94, 84);
+            this.Eyetracker.TabIndex = 0;
+            this.Eyetracker.UseVisualStyleBackColor = false;
+            this.Eyetracker.Click += new System.EventHandler(this.Eyetracker_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(617, 125);
+            this.Controls.Add(this.Closebt);
+            this.Controls.Add(this.AlwaysTop);
+            this.Controls.Add(this.Keyboard);
+            this.Controls.Add(this.Narrator);
+            this.Controls.Add(this.Magnify);
+            this.Controls.Add(this.Eyetracker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(1300, 910);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Eyetracker;
+        private System.Windows.Forms.Button Magnify;
+        private System.Windows.Forms.Button Narrator;
+        private System.Windows.Forms.Button Keyboard;
+        private System.Windows.Forms.Button AlwaysTop;
+        private System.Windows.Forms.Button Closebt;
+    }
+}
+
---
+++ Source/NoonUI/NoonUI/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace NoonUI
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int WM_LBUTTONDOWN = 0x0201;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+
+        private HashSet<Control> controlsToMove = new HashSet<Control>();
+
+        static bool eyePointing = false;
+        Process process;
+        public Form1()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+
+            controlsToMove.Add(this);
+            IntPtr wow64Value = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref wow64Value);
+
+        }
+
+        //아이트래커 실행
+        private void Eyetracker_Click(object sender, EventArgs e)
+        {
+            eyePointing = !eyePointing;
+            if (eyePointing)
+            {
+                process = Process.Start(@"C:\\Users\Song\Desktop\source\eye-tracking-zoom\eye-tracking-zoom\bin\Debug\eye-tracking-zoom.exe"); //시연시에 바꿔야됨
+                Eyetracker.BackColor = Color.LightGray;
+            }
+            else
+            {
+                process.Kill();
+                Eyetracker.BackColor = Color.Transparent;
+            }
+        }
+
+        //돋보기 실행
+        private void Magnify_Click(object sender, EventArgs e)
+        {
+            Process[] handler = Process.GetProcessesByName("Magnify");
+            if (handler.Length == 0)
+            {
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.FileName = @"Magnify.exe";
+                Magnify.BackColor = Color.LightGray;
+                Process.Start(process);
+            }
+            else
+            {
+                handler[0].Kill();
+                Magnify.BackColor = Color.Transparent;
+            }
+        }
+
+        //내레이터 실행
+        private void Narrator_Click(object sender, EventArgs e)
+        {
+            Process[] handler = Process.GetProcessesByName("Narrator");
+            if (handler.Length == 0)
+            {
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.FileName = @"Narrator.exe";
+                Narrator.BackColor = Color.LightGray;
+                Process.Start(process);
+            }
+            else
+            {
+                Narrator.BackColor = Color.Transparent;
+                handler[0].Kill();
+            }
+        }
+        
+        //화상키보드 실행
+        private void Keyboard_Click(object sender, EventArgs e)
+        {
+            Process[] handler = Process.GetProcessesByName("osk");
+            if (handler.Length == 0)
+            {
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.FileName = @"osk.exe";
+                Keyboard.BackColor = Color.LightGray;
+                Process.Start(process);
+            }
+            else
+            {
+                Keyboard.BackColor = Color.Transparent;
+                handler[0].Kill();
+            }
+        }
+
+        //항상위 실행
+        private void AlwaysTop_Click(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+            if (TopMost)
+            { AlwaysTop.BackColor = Color.LightGray; }
+            else
+            { AlwaysTop.BackColor = Color.Transparent; }
+        }
+
+        //닫기버튼
+        private void Closebt_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName("Magnify").Length > 0)
+            { Magnify.BackColor = Color.LightGray; }
+            else { Magnify.BackColor = Color.Transparent; }
+
+            if (Process.GetProcessesByName("Narrator").Length > 0)
+            { Narrator.BackColor = Color.LightGray; }
+            else { Narrator.BackColor = Color.Transparent; }
+
+            if (Process.GetProcessesByName("osk").Length > 0)
+            { Keyboard.BackColor = Color.LightGray; }
+            else { Keyboard.BackColor = Color.Transparent; }
+        }
+
+        public bool PreFilterMessage(ref System.Windows.Forms.Message message)
+        {
+            if (message.Msg == WM_LBUTTONDOWN &&
+                 controlsToMove.Contains(Control.FromHandle(message.HWnd)))
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                return true;
+            }
+            return false;
+        }
+    }
+}
